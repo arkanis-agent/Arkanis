d7822a7bfdb809dda9ad1e802185779bd27566b4
Diego <dndazevedo@hotmail.com>
2026-04-05 01:39:34 -0300

    Auto-evolution: Refatoração e Boas Práticas em optimize_memory_usage

--- Arkanis_V3/short_term.py
+++ Arkanis_V3/short_term.py
@@ -1,4 +1,5 @@
-def optimize_memory_usage():
-	import gc
-	gc.collect()
-	return True
+import gc
+
+def optimize_memory_usage() -> int:
+    """Força a coleta de lixo e retorna a quantidade de objetos coletados."""
+    return gc.collect()
