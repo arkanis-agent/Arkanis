4994efb903d49c86e7c32b8313480b4a7a3c3ad5
Diego <dndazevedo@hotmail.com>
2026-04-05 00:11:51 -0300

    Auto-evolution: Inicialização Padrão para Pacote de Interfaces

---
+++ interfaces/__init__.py
@@ -0,0 +1,11 @@
+"""
+Módulo de Interfaces do Sistema Arkanis V3
+
+Este pacote define todas as interfaces públicas do sistema,
+incluindo contratos de serviço, protocolos e APIs.
+"""
+
+__all__ = []  # Lista explícita de exports públicos
+
+# Inicializações do pacote
+print(f'Inicializando interfaces Arkanis V3 ({__name__})...')
